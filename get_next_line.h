5e2dbbefc84c193d5b8e0df643b0dfade2cde078
nicolasgsantana <nicolas07san@gmail.com>
2025-08-04 11:55:07 -0300

    add header and necessary prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 10:09:15 by nde-sant          #+#    #+#             */
+/*   Updated: 2025/08/04 11:27:39 by nde-sant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 #endif
@@ -7,3 +19,10 @@
 #endif
 
 #include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memchr(const void *s, int c, size_t n);
+char *get_next_line(int fd);
