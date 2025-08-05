623387c0a665a9c791d844aa062952baa1082d81
nicolasgsantana <nicolas07san@gmail.com>
2025-08-05 13:24:53 -0300

    update to norm

--- get_next_line.h
+++ get_next_line.h
@@ -6,25 +6,26 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 10:09:15 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/08/04 13:14:33 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/08/05 13:24:21 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-#endif
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-#include <unistd.h>
-#include <stddef.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdlib.h>
 
 size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strdup(const char *s);
 char	*get_first_line(const char *s);
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+
+#endif
