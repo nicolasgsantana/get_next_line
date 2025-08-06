cdeb74bd00dccb4766b5018e02ab5a5af0437e54
nicolasgsantana <nicolas07san@gmail.com>
2025-08-06 12:35:23 -0300

    refactor code for readability

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 12:47:08 by nicolas           #+#    #+#             */
-/*   Updated: 2025/08/05 16:06:17 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/08/06 12:28:45 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,47 @@
 
 static char	*get_first_line(const char *s)
 {
-	char	*line;
-	int		line_size;
-	int		i;
+	int		line_len;
 
-	line_size = 0;
-	while (s[line_size] && s[line_size] != '\n')
-		line_size++;
-	if (s[line_size] == '\n')
-		line_size++;
-	line = malloc((line_size + 1) * sizeof(char));
-	if (!line)
-		return (NULL);
-	i = 0;
-	while (i < line_size)
-	{
-		line[i] = s[i];
-		i++;
-	}
-	line[i] = '\0';
-	return (line);
+	line_len = 0;
+	while (s[line_len] && s[line_len] != '\n')
+		line_len++;
+	if (s[line_len] == '\n')
+		line_len++;
+	return (ft_substr(s, 0, line_len));
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*line;
-	char		*buf_str;
+	static char	*stack;
 	char		*temp;
+	char		*buffer;
+	char		*new_line;
 	ssize_t		bytes_read;
-
-	buf_str = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if(!buf_str)
-		return (NULL);
-	bytes_read = read(fd, &buf_str, BUFFER_SIZE);
-	if (!line)
-		line = ft_strdup("");
-	else if(ft_memchr(line, '\n', ft_strlen(line)))
+	
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return(NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (!stack)
+		stack = ft_strdup("");
+	new_line = ft_strchr(stack, '\n');
+	if (new_line)
 	{
-		temp = line;
-		line = ft_strdup((char *)ft_memchr(temp, '\n', ft_strlen(temp)) + 1);
+		temp = stack;
+		stack = ft_strdup(new_line + 1);
 		free(temp);
-	}
-	else if (bytes_read <= 0)
-	{
-		free(buf_str);
-		free(line);
-		return (NULL);
 	}
 	while (bytes_read > 0)
 	{
-		buf_str[bytes_read] = '\0';
-		temp = line;
-		line = ft_strjoin(temp, buf_str);
+		buffer[bytes_read] = '\0';
+		temp = stack;
+		stack = ft_strjoin(temp, buffer);
 		free(temp);
-		if (ft_memchr(buf_str, '\n', BUFFER_SIZE))
-			break ;
-		bytes_read = read(fd, &buf_str, BUFFER_SIZE);
+		if (ft_strchr(buffer, '\n'))
+			break;
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
 	}
-	free(buf_str);
-	return (get_first_line(line));
+	free(buffer);
+	return (get_first_line(stack));
 }
